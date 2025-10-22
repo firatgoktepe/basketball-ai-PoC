4f846bc6ca8263117c2903cf08e74e1e79ea3220
firatgoktepe <firatgoktepe1986@gmail.com>
2025-10-22 15:49:02 +0300

    feat: add vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
